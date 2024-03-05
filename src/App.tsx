418e7ac137391718c3c4c23e73e6ca1c4313e871
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-03-05 17:42:59 +0100

    chore: remove Bookmark component and imports from App template

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import BookmarkIcon from "./components/Features/Bookmark/BookmarkIcon/BookmarkIcon"
-import BookmarksButton from "./components/Features/Bookmark/BookmarksButton/BookmarksButton"
-
 function App() {
-  return (
-    <>
-      <BookmarkIcon />
-    </>
-  )
+  return <></>
 }
 
 export default App
