3238457cba343cd46d3b953793b8f895ec0aa0ee
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-03-20 21:16:50 +0100

    feat: add results count data to ResultsCount component

--- src/components/ResultsCount/ResultsCount.tsx
+++ src/components/ResultsCount/ResultsCount.tsx
@@ -1,5 +1,15 @@
 import styles from "./ResultsCount.module.css"
 
-export default function ResultsCount() {
-  return <p className={styles.count}>0 results</p>
+type ResultsCountProps = {
+  totalNumberOfResults: number
+}
+
+export default function ResultsCount({
+  totalNumberOfResults
+}: ResultsCountProps) {
+  return (
+    <p className={styles.count}>
+      <span className="u-bold">{totalNumberOfResults}</span> results
+    </p>
+  )
 }
--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -46,6 +46,7 @@ export const useJobItems = (searchText: string) => {
   const [jobItems, setJobItems] = useState<JobItem[]>([])
   const [isLoading, setIsLoading] = useState(false)
 
+  const totalNumberOfResults = jobItems.length
   const jobItemsSliced = jobItems.slice(0, 7)
 
   useEffect(() => {
@@ -61,5 +62,5 @@ export const useJobItems = (searchText: string) => {
     fetchData()
   }, [searchText])
 
-  return [jobItemsSliced, isLoading] as const
+  return [jobItemsSliced, isLoading, totalNumberOfResults] as const
 }
