faa7240d5a69cffe48bb40f5278b6c02bac13383
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-03-22 09:35:28 +0100

    feat: add fetchJobItem abstraction to useJobItem Hook

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -3,14 +3,16 @@ import { BASE_API_URL } from "./consts"
 import { JobItem } from "./types"
 import { useQuery } from "@tanstack/react-query"
 
+const fetchJobItem = async (id: number) => {
+  const response = await fetch(`${BASE_API_URL}/${id}`)
+  const data = await response.json()
+  return data
+}
+
 export function useJobItem(id: number | null) {
   const { data, isLoading } = useQuery(
     ["job-item", id],
-    async () => {
-      const response = await fetch(`${BASE_API_URL}/${id}`)
-      const data = await response.json()
-      return data
-    },
+    () => (id ? fetchJobItem(id) : null),
     {
       staleTime: 1000 * 60 * 60,
       refetchOnWindowFocus: false,
@@ -19,7 +21,7 @@ export function useJobItem(id: number | null) {
       onError: () => {}
     }
   )
-  return [data.jobItem, isLoading] as const
+  return [data?.jobItem, isLoading] as const
 }
 
 export const useJobItems = (searchText: string) => {
