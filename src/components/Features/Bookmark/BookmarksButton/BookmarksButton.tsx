fc6f4f0ab031874e6cc5b8c802bbedfa06a27e78
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-03-29 14:01:43 +0100

    refactor: create custom hook for popover close on outside functionality

--- src/components/Features/Bookmark/BookmarksButton/BookmarksButton.tsx
+++ src/components/Features/Bookmark/BookmarksButton/BookmarksButton.tsx
@@ -1,28 +1,15 @@
 import { TriangleDownIcon } from "@radix-ui/react-icons"
 import styles from "./BookmarksButton.module.css"
 import BookmarksPopover from "../BookmarksPopover/BookmarksPopover"
-import { useEffect, useRef, useState } from "react"
+import { useRef, useState } from "react"
+import { useOnClickOutside } from "../../../../lib/hooks"
 
 export default function BookmarksButton() {
   const [isOpen, setIsOpen] = useState(false)
   const buttonRef = useRef<HTMLButtonElement>(null)
-  const popOverRef = useRef<HTMLDivElement>(null)
+  const popoverRef = useRef<HTMLDivElement>(null)
 
-  useEffect(() => {
-    const handleClick = (e: MouseEvent) => {
-      if (
-        e.target instanceof HTMLElement &&
-        !buttonRef.current?.contains(e.target) &&
-        !popOverRef.current?.contains(e.target)
-      ) {
-        setIsOpen(false)
-      }
-    }
-
-    document.addEventListener("click", handleClick)
-
-    return () => document.removeEventListener("click", handleClick)
-  }, [])
+  useOnClickOutside([buttonRef, popoverRef], () => setIsOpen(false))
 
   return (
     <section>
@@ -33,7 +20,7 @@ export default function BookmarksButton() {
       >
         Bookmarks <TriangleDownIcon />
       </button>
-      {isOpen && <BookmarksPopover ref={popOverRef} />}
+      {isOpen && <BookmarksPopover ref={popoverRef} />}
     </section>
   )
 }
