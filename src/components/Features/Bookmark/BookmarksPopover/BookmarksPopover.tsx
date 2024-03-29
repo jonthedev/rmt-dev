11ea1353a2b6a53a75c06d78f91e6142c788464e
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-03-29 14:10:00 +0100

    feat: create portal for BookmarksPopover

--- src/components/Features/Bookmark/BookmarksPopover/BookmarksPopover.tsx
+++ src/components/Features/Bookmark/BookmarksPopover/BookmarksPopover.tsx
@@ -2,13 +2,15 @@ import JobList from "../../Job/JobList/JobList"
 import styles from "./BookmarksPopover.module.css"
 import { useBookmarksContext } from "../../../../lib/hooks"
 import { forwardRef } from "react"
+import { createPortal } from "react-dom"
 
 const BookmarksPopover = forwardRef<HTMLDivElement>(function (_, ref) {
   const { bookmarkedJobItems, isLoading } = useBookmarksContext()
-  return (
+  return createPortal(
     <div ref={ref} className={styles["bookmarks-popover"]}>
       <JobList isLoading={isLoading} jobItems={bookmarkedJobItems} />
-    </div>
+    </div>,
+    document.body
   )
 })
 
